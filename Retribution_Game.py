fa756dca4d04c183e990bb83c6a1c1f19aecfd98
NickJacksonpyah <s137665@gmail.com>
2014-12-01 14:14:42 -0600

    STORY2
    more story done :)

--- Retribution_Game.py
+++ Retribution_Game.py
@@ -13,3 +13,10 @@ name = str(raw_input())
 print "So your name is " + name + "?"
 print " alright " + name " here is your job, you are to go to Felador, about a mile away.
 print " about 15-20 minutes later " +name "appears at Felador, interested in why he is here."
+print " he walks up to a castle, there are 2 guards..."
+print " with the muffleness of the mask, the guard asks, who comes here?
+print " my name is " + name, "i got told to come here as i am now on probation i was told to come here to retrieve my item for the final present."
+print " which prison do you come from? "
+print " regolith prison "
+print " fine, you have 1 chance. we have our eyes on you" + name
+print " you open the huge door and walk thorugh the huge door"                                      
