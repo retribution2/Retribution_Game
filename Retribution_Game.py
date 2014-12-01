4d0153397bde02f402ceb97ee883316d3306b6e6
DrakeC <s581300@student.roundrockisd.org>
2014-12-01 13:13:06 -0600

    Added the game file.

---
+++ Retribution_Game.py
@@ -0,0 +1,13 @@
+# retribution
+#print "Congratulation,you are on probation period, now until you have done gotten the item, you will be a free man. task you have completed this task. You can be free whenever you have completed this task"
+#print "ok sir!"
+
+
+#from ImageLibrary import *
+#myfile=pickAFile("\Users\Public\Pictures\Sample Pictures\Ff14-class-archer.jpg")
+#pict=makePicture("\Users\Public\Pictures\Sample Pictures\Ff14-class-archer.jpg")
+#show(pict)
+            
+#print "you open up the cell doors and walk out.. thinking about what you have just been told"
+name = str(raw_input())
+print "So your name is " + name + "?"
