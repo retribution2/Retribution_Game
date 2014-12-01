416427e6be11ca8e9de6cc115f5b6d8be14cd05e
NickJacksonpyah <s137665@gmail.com>
2014-12-01 13:43:15 -0600

    story
    added to story

--- Retribution_Game.py
+++ Retribution_Game.py
@@ -11,3 +11,5 @@
 #print "you open up the cell doors and walk out.. thinking about what you have just been told"
 name = str(raw_input())
 print "So your name is " + name + "?"
+print " alright " + name " here is your job, you are to go to Felador, about a mile away.
+print " about 15-20 minutes later " +name "appears at Felador, interested in why he is here."
