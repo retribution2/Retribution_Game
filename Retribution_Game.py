8d608f004fb6dc66ba79d948ea8b23c02a0207be
NickJacksonpyah <s137665@gmail.com>
2014-12-03 13:58:13 -0600

    12/3
    more story.

--- Retribution_Game.py
+++ Retribution_Game.py
@@ -12,14 +12,50 @@
 #print "you open up the cell doors and walk out.. thinking about what you have just been told"
 name = str(raw_input())
 print "So your name is " + name + "?"
-print " alright " + name " here is your job, you are to go to Felador, about a mile away.
-print " about 15-20 minutes later " +name "appears at Felador, interested in why he is here."
-print " he walks up to a castle, there are 2 guards..."
-print " with the muffleness of the mask, the guard asks, who comes here?
-print " my name is " + name, "i got told to come here as i am now on probation i was told to come here to retrieve my item for the final present."
-print " which prison do you come from? "
-print " regolith prison "
-print " fine, you have 1 chance. we have our eyes on you" + name
-print " you open the huge door and walk thorugh the huge door"                                      
+userInput = raw_input()
+if userInput == "":   
 
-
+    print " alright " + name + " here is your job, you are to go to Felador, about a mile away."
+userInput = raw_input()
+if userInput == "":
+    
+    print " about 15-20 minutes later " +name + "appears at Felador, interested in why he is here."
+    print " he walks up to a castle, there are 2 guards..."
+    print " with the muffleness of the mask, the guard asks, who comes here?"
+    print " my name is " + name, "i got told to come here as i am now on probation i was told to come here to retrieve my item for the final present."
+    print " which prison do you come from? "
+userInput = raw_input()
+if userInput == "":
+    
+    print " regolith prison "
+userInput = raw_input()
+if userInput == "":
+   
+    print " fine, you have 1 chance. we have our eyes on you" + name
+userInput = raw_input()
+if userInput == "":
+    
+    print " you open the huge door and walk thorugh the huge door"      
+userInput = raw_input()
+if userInput == "":
+                            
+    print " you can smell something but you disregard it.. but you follow the map to"
+    userInput = raw_input()
+if userInput == "":
+    print ""
+    print "you walk for around 10 minutes, following the map, and you finally find something interesting.."
+    userInput = raw_input()
+if userInput == "":
+    print ""
+    print "you find a grunt on the floor..."
+    userInput = raw_input()
+if userInput == "":
+    print ""
+print "it's already bleeding out on the floor from the previos person.. and you just step on it to finish it off."
+print ""
+print ""
+print ""
+print ""
+userInput = raw_input()
+if userInput == "":
+    print ""
